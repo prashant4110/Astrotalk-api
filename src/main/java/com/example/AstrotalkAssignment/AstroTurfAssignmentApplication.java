bd8a202e4cd0007c96ba44d1a1819bf42d38095e
Prashant Gupta <pk.pupri@gmail.com>
2023-11-25 11:55:47 +0530

    patient table created

--- src/main/java/com/example/AstrotalkAssignment/AstroTurfAssignmentApplication.java
+++ src/main/java/com/example/AstrotalkAssignment/AstroTurfAssignmentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AstroTurfAssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AstroTurfAssignmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AstroTurfAssignmentApplication.class, args);
+    }
 
 }
