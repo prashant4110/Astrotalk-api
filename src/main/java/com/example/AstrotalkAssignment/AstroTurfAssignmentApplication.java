feb789ab84c7a0ea5dfd81d229f65bc9bdb8acd8
Prashant Gupta <pk.pupri@gmail.com>
2023-11-25 10:16:18 +0530

    initialize spring boot application with dependency of sql-server,starter etc

---
+++ src/main/java/com/example/AstrotalkAssignment/AstroTurfAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.AstrotalkAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroTurfAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroTurfAssignmentApplication.class, args);
+	}
+
+}
